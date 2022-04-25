138275a369c2b59487ac6f5fc195e3c4194a4b40
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2022-04-24 22:24:18 -0400

    add images, state and SingleCard component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { useState } from 'react'
 import './App.css';
 
+import SingleCard from './components/SingleCard'
+
+const cardImages = [ //created outside of component since they never need to change and to avoid array recreation every time the component is re-evaluated
+  { 'src': '/img/helmet-1.png' },
+  { 'src': '/img/potion-1.png' },
+  { 'src': '/img/ring-1.png' },
+  { 'src': '/img/scroll-1.png' },
+  { 'src': '/img/shield-1.png' },
+  { 'src': '/img/sword-1.png' }
+]
+
 function App() {
+
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  //shuffle cards and apply random id to each card
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5) //sort in JS is the stupidest sort in all coding languages, compares UTF-16 code units values for strings so 80 is less than 8, has to be used with compare function which returns <0(sort ascending), >0(sort descending), ===0(keep original order). We are using it as random sort in this case.
+      .map(card => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className='card-grid'>
+        {cards.map(card => (
+          <SingleCard card={card} key={card.id} />
+        ))}
+      </div>
     </div>
   );
 }
